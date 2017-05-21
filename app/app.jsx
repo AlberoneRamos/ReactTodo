1a362ee6dfbd4b95bca9cacc86ef05adc6f6ea93
DJDiney <rickeight@hotmail.com>
2017-05-21 17:26:06 -0300

    Aplicativo iniciado

---
+++ app/app.jsx
@@ -0,0 +1,10 @@
+import Inferno from 'inferno';
+import 'style-loader!foundation-sites/dist/css/foundation.min.css';
+import 'style-loader!css-loader!sass-loader!./styles/app.scss';
+import TodoApp from './components/TodoApp.jsx';
+$(document).foundation();
+
+Inferno.render(
+    <TodoApp/>,
+    document.getElementById('app')
+);
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,12 @@
+import Inferno from 'inferno';
+import Component from 'inferno-component';
+
+export default class TodoApp extends Component{
+    render(){
+        return(
+            <div>
+                <h1>TodoApp.jsx</h1>
+            </div>
+        );
+    }
+}
