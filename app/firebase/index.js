88a6237ddfd3bfe9c3abc667aa562264f7e4c2b4
DJDiney <rickeight@hotmail.com>
2017-06-03 12:38:22 -0300

    Criada uma base de testes e uma de produção. Credenciais do Firebase agora são variáveis de ambiente

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,13 +2,14 @@ import firebase from 'firebase';
 
 try{
     var config = {
-        apiKey: "AIzaSyDStMnSzde5xQBA0mjFwOrpxjQPY4KxEPM",
-        authDomain: "todoapp-3b4bc.firebaseapp.com",
-        databaseURL: "https://todoapp-3b4bc.firebaseio.com",
-        projectId: "todoapp-3b4bc",
-        storageBucket: "todoapp-3b4bc.appspot.com",
-        messagingSenderId: "945095260258"
+        apiKey:process.env.API_KEY,
+        authDomain:process.env.AUTH_DOMAIN,
+        databaseURL:process.env.DATABASE_URL,
+        projectId:process.env.PROJECT_ID,
+        storageBucket:process.env.STORAGE_BUCKET,
+        messagingSenderId:process.env.MESSAGING_SENDER_ID
     };
+    console.log(config);
     firebase.initializeApp(config);
 } catch(e){
 
